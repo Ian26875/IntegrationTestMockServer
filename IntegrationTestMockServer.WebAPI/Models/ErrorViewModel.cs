60081841721e000a3f5ab49382b563ca4af1e64a
Ian <fds26875@gmail.com>
2024-09-29 12:28:15 +0800

    Add HomeController and basic views
    This commit introduces the HomeController along with Index, Privacy, and Error actions. It also includes basic views for these actions and supporting styles and scripts. Additionally, site layout and structure have been set up with Bootstrap and custom CSS.

---
+++ IntegrationTestMockServer.WebAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IntegrationTestMockServer.WebAPI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
